4108f051b4053e62470e9d3cf5cf7336868db7db
Loïs Bilat <lois@bilat.xyz>
2019-10-10 22:09:37 +0200

    Tested superresolution

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ net = Net(8, verbose = 0)
 
 
 filename = "/mnt/Data/maestro-v2.0.0/2004/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav"
-filename = "../MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav"
-#train_data = AudioUpScalingDataset(filename, window=1024, stride=128, samples=128, compressed_rate=5000, target_rate=10000, start=10000)
-train_data = AudioIDDataset(filename, window=1024, stride=128, samples=1000, start=10000)
+#filename = "../MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav"
+train_data = AudioUpScalingDataset(filename, window=1024, stride=128, samples=10000, compressed_rate=5000, target_rate=10000, start=10000)
+#train_data = AudioIDDataset(filename, window=1024, stride=128, samples=10000, start=10000)
 train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
 train_step = make_train_step(net, nn.MSELoss(), optim.Adam(net.parameters(), lr=0.0001))
 
@@ -49,8 +49,8 @@ for epoch in range(n_epochs):
         
 #%%
 
-#test_data = AudioUpScalingDataset(filename, window=1024, stride=1024, samples=32, compressed_rate=5000, target_rate=10000)
-test_data = AudioIDDataset(filename, window=1024, stride=128, samples=1000)
+test_data = AudioUpScalingDataset(filename, window=1024, stride=1024, samples=1000, compressed_rate=5000, target_rate=10000)
+#test_data = AudioIDDataset(filename, window=1024, stride=1024, samples=1000)
 test_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False)
 test_step = make_test_step(net, nn.MSELoss())
 
@@ -74,4 +74,6 @@ out = concat_list_tensors(outputs)
 
 out_formated = out.reshape((1, out.size()[2]))
 print(out_formated.size())
-torchaudio.save("out.wav", out_formated, 44100, precision=16, channels_first=True)
+torchaudio.save("out.wav", out_formated, 10000, precision=16, channels_first=True)
+
+#%%
